af8fe364f4fd5ff7379eb3119f71cb63418d2117
DeeptiDiddi <deeptididdi@gmail.com>
2026-08-18 18:23:56 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,11 @@
+public class assignment{
+ public static void main(String[] arg){
+  int a;
+  System.out.println(a=5);
+  System.out.println(a+=1);
+  System.out.println(a*=3);
+  System.out.println(a>>=2);
+  System.out.println(a<<=2);
+
+ }
+}
