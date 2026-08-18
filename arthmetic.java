92e0bc76a87918e77988bfe4ce89779c80b5f045
DeeptiDiddi <deeptididdi@gmail.com>
2026-08-18 18:24:20 +0530

    Add files via upload

---
+++ arthmetic.java
@@ -0,0 +1,11 @@
+public class arthmetic{
+ public static void main(String[] arg){
+  int a=10; 
+  int b=20;
+  System.out.println(a+b);
+  System.out.println(a-b);
+  System.out.println(a*b);
+  System.out.println(a/b);
+
+ }
+}
