bb6a29a388eeb5ea96b07dfe35f1901c36f8d417
DeeptiDiddi <deeptididdi@gmail.com>
2026-08-19 18:00:37 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,14 @@
+public class bitwise{
+ public static void main(String[] arg){
+  int a;
+  int b=5;
+  System.out.println(a=1);
+  System.out.println(a|b);
+  System.out.println(a&b);
+  System.out.println(a&=1);
+  System.out.println(a&=2);
+  System.out.println(a|=1);
+
+
+ }
+}
---
+++ ifcon.java
@@ -0,0 +1,9 @@
+public class ifcon{
+ public static void main(String[] arg){
+  int a=10;
+  if (a>0){
+  System.out.println("positive");
+  }
+  System.out.println("hi");
+ }
+}
