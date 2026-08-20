b96d5c8294b6578d6f9a6781f3c9c9e64174c7c3
DeeptiDiddi <deeptididdi@gmail.com>
2026-08-20 20:49:32 +0530

    Add files via upload

---
+++ elseif.java
@@ -0,0 +1,15 @@
+public class elseif{
+ public static void main(String[] arg){
+  int a=-5;
+  if (a>0){
+  System.out.println("positive");
+  }
+  else if(a<0){
+    System.out.println("negative");
+  }
+  else{
+  System.out.println("0");
+ }
+ System.out.println("hi");
+}
+}
