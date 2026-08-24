d048d383392ddf411e0c60daf6da7369b2e660c3
DeeptiDiddi <deeptididdi@gmail.com>
2026-08-24 19:48:53 +0530

    Add files via upload

---
+++ switch1.java
@@ -0,0 +1,19 @@
+public class switch1{
+ public static void main(String[] arg){
+  int a=3;
+  switch(a)
+  {
+   case 1:
+    System.out.println("One");
+    break;
+   case 2:
+    System.out.println("Two");
+    break;
+   case 3:
+    System.out.println("Three");
+    break;
+   default:
+    System.out.println("Other");
+  }
+ }
+}
---
+++ whilee.java
@@ -0,0 +1,9 @@
+public class whilee{
+ public static void main(String[] arg){
+  int a=3;
+  while (a<=5){
+   System.out.println(a);
+   a++;
+  }
+}
+}
