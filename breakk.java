c722f58d44e5459553caf5e6b45a3f7273af5d0f
DeeptiDiddi <deeptididdi@gmail.com>
2026-08-24 19:52:48 +0530

    Add files via upload

---
+++ breakk.java
@@ -0,0 +1,11 @@
+public class breakk{
+ public static void main(String[] arg){
+   for(int a=0;a<=5;a++){
+      if(a==3){
+         break;
+      }
+    System.out.println(a);
+   }
+ System.out.println("hi");
+}
+}
---
+++ continuee.java
@@ -0,0 +1,11 @@
+public class continuee{
+ public static void main(String[] arg){
+   for(int a=0;a<=5;a++){
+      if(a==3){
+         continue;
+      }
+    System.out.println(a);
+   }
+ System.out.println("hi");
+}
+}
