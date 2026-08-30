63e48493297206984f96a96774ec6494ede8d993
DeeptiDiddi <deeptididdi@gmail.com>
2026-08-30 21:52:58 +0530

    Add files via upload

---
+++ bitorlog.java
@@ -0,0 +1,10 @@
+public class bitorlog{
+ public static void main(String[] arg){
+  int a=10;
+  int b=6;
+  System.out.println(a>b||a++<b);
+  System.out.println(a);
+  System.out.println(a>b|a++<b);
+  System.out.println(a);
+ }
+}
