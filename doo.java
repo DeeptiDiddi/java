3996144316bd69055f38722d30d25fe83b95b55d
DeeptiDiddi <deeptididdi@gmail.com>
2026-09-09 13:36:24 +0530

    Add files via upload

---
+++ doo.java
@@ -0,0 +1,10 @@
+public class doo{
+ public static void main(String[] arg){
+   int a=2;
+   do{ 
+    System.out.println(a);
+    a++;
+   }while (a<=5);
+  
+}
+}
---
+++ implicit.java
@@ -0,0 +1,8 @@
+public class implicit{
+   public static void main(String[] arg){
+    int a=10;
+    float b=20;
+    System.out.println(a);
+    System.out.println(b);
+   }
+}
