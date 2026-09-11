9395d6a3a7a685d2a1e18f688253e40953330911
DeeptiDiddi <deeptididdi@gmail.com>
2026-09-11 13:25:03 +0530

    Add files via upload

---
+++ unaryincredecre.java
@@ -0,0 +1,15 @@
+public class unaryincredecre{
+ public static void main(String[] arg){
+  int a=10; 
+  System.out.println(a++);
+  System.out.println(a);
+  System.out.println(++a);
+  System.out.println(a);
+  System.out.println(a--);
+  System.out.println(a);
+  System.out.println(--a);
+  System.out.println(a);
+ 
+
+ }
+}
