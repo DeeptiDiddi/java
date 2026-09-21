75b6ae3b1a51162d0fd682e8b1f0aa28a32a68b1
DeeptiDiddi <deeptididdi@gmail.com>
2026-09-21 18:10:59 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,10 @@
+class reverse{
+    public static void main(String[] args) {
+      int[] nums={10, 20,30,40,50};
+      for(int i=nums.length-1;i>=0;i--){
+         System.out.print(nums[i] + " ");      
+        }
+     
+    }
+}
+    
